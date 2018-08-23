a372e1d85284170190e9418d1ef5581d18c01298
EaglesRoboticsTeam <38738763+EaglesRoboticsTeam@users.noreply.github.com>
2018-08-22 23:43:13 -0300

    Create TCS3200.py

---
+++ TCS3200.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import time
+
+s2 = 23
+s3 = 24
+signal = 25
+NUM_CYCLES = 10
+
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(s2,GPIO.OUT)
+  GPIO.setup(s3,GPIO.OUT)
+  print("\n")
+
+def loop():
+  temp = 1
+  while(1):  
+
+    #Vermelho
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.LOW)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start 
+    red  = NUM_CYCLES / duration   
+
+    #Azul
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    blue = NUM_CYCLES / duration
+
+    #Verde
+    GPIO.output(s2,GPIO.HIGH)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    green = NUM_CYCLES / duration
+    
+    if (red < 12000) and (blue < 12000) and (green > 12000):
+        print("Green")
+        temp = 1
+    else:
+        print("nove")
+
+def endprogram():
+    GPIO.cleanup()
+
+if __name__=='__main__':
+    
+    setup()
+
+    try:
+        loop()
+
+    except KeyboardInterrupt:
+endprogram()
